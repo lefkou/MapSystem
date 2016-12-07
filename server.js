d2096a5ed71369379ed7ce1204e5fff2c5629866
lefkou <elekousis@gmail.com>
2016-12-07 15:48:45 +0000

    Added connection to the mongoDB database in mongolab.

--- server.js
+++ server.js
@@ -1,10 +1,4 @@
 /**
  * Created by lef on 07/12/2016.
  */
-var http = require('http')
-var port = process.env.PORT || 1337;
-
-http.createServer(function(request, response) {
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.end('Hello World\n');
-}).listen(port);
+require('./connect');
