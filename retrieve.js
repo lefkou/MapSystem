226a458bf9c7cd25a1d6b7c54d63588e145b090a
lefkou <elekousis@gmail.com>
2016-12-10 12:06:35 +0000

    Retrieve points from db.

--- retrieve.js
+++ retrieve.js
@@ -28,22 +28,23 @@ http.createServer(function(request, response) {
 
             // do some work here with the database.
             var collection = db.collection('points');
-            var results = collection.find({name: 'Walk 1'});
+            var results = collection.findOne();
             // iterate
-            results.each(function (err, result) {
-                //if the result is null, there are no more results, it’s ok to close everything
-                if (result == null) {
-                    response.end('Completed');
-                    db.close();
-                }
-                if (err) {
-                    response.write(err);
-                } else {
-                    response.write('Nade');
-                    response.write('Fetched: ' + result.name + ' : ' + result.length +'\n');
-                }
-
-            });
+            response.write(''+ results.length);
+            // results.each(function (err, result) {
+            //     //if the result is null, there are no more results, it’s ok to close everything
+            //     if (result == null) {
+            //         response.end('Completed');
+            //         db.close();
+            //     }
+            //     if (err) {
+            //         response.write(err);
+            //     } else {
+            //         response.write('Nade');
+            //         response.write('Fetched: ' + result.name + ' : ' + result.length +'\n');
+            //     }
+            //
+            // });
 
             //Done Close connection
             db.close();
