1f71fc24fd97b2f7a91b1c1cebf2fec1f10f23fd
lefkou <elekousis@gmail.com>
2016-12-10 16:10:39 +0000

    UI changes

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/points', users);
+app.use('/api/points', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
