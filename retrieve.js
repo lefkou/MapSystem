8c479cc9fdc47d5bca91c0cd4b2c5f145f3af902
lefkou <elekousis@gmail.com>
2016-12-10 12:11:47 +0000

    Retrieve points from db.

--- retrieve.js
+++ retrieve.js
@@ -28,7 +28,7 @@ http.createServer(function(request, response) {
 
             // do some work here with the database.
             var collection = db.collection('points');
-            var results = collection.findOne();
+            var results = collection.find();
             // iterate
             // response.write(''+ results.length);
             results.each(function (err, result) {
